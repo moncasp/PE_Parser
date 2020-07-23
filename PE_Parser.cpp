bee01b446854cefba55e9780a6e8422fa5027d6c
Murat ERDEM <muraterdemcs@gmail.com>
2020-07-23 10:12:55 +0300

    MZ Header bilgilerinin çıkartılması

--- PE_Parser.cpp
+++ PE_Parser.cpp
@@ -3,6 +3,34 @@
 #include<locale.h>
 using namespace std;
 
+
+
+void PImageHeaderWriter(PIMAGE_DOS_HEADER PImageHeader) {
+	cout << endl << "______PIMAGE_DOS_HEADER______" << endl;
+	cout << "e_magic : " << hex << PImageHeader->e_magic << endl;
+	cout << "e_cblp : " << hex << PImageHeader->e_cblp << endl;
+	cout << "e_cp : " << hex << PImageHeader->e_cp << endl;
+	cout << "e_crlc : " << hex << PImageHeader->e_crlc << endl;
+	cout << "e_cparhdr : " << hex << PImageHeader->e_cparhdr << endl;
+	cout << "e_minalloc : " << hex << PImageHeader->e_minalloc << endl;
+	cout << "e_maxalloc : " << hex << PImageHeader->e_maxalloc << endl;
+	cout << "e_ss : " << hex << PImageHeader->e_ss << endl;
+	cout << "e_sp : " << hex << PImageHeader->e_sp << endl;
+	cout << "e_csum : " << hex << PImageHeader->e_csum << endl;
+	cout << "e_ip : " << hex << PImageHeader->e_ip << endl;
+	cout << "e_cs : " << hex << PImageHeader->e_cs << endl;
+	cout << "e_lfarlc : " << hex << PImageHeader->e_lfarlc << endl;
+	cout << "e_ovno : " << hex << PImageHeader->e_ovno << endl;
+	cout << "e_res : " << hex << PImageHeader->e_res[3] << endl;
+	cout << "e_oemid : " << hex << PImageHeader->e_oemid << endl;
+	cout << "e_oeminfo : " << hex << PImageHeader->e_oeminfo << endl;
+	cout << "e_res2 : " << hex << PImageHeader->e_res2[9] << endl;
+	cout << "e_lfanew : " << hex << PImageHeader->e_lfanew << endl;
+	cout << endl << endl << endl;
+}
+
+
+
 int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, "Turkish");
@@ -37,6 +65,10 @@ int main(int argc, char* argv[])
 	DWORD bytesRead;
 
 	ReadFile(fileHandle, fileData, fileSize, &bytesRead, NULL);
+
+	
+	PIMAGE_DOS_HEADER PImageHeader = (PIMAGE_DOS_HEADER)fileData;
+	PImageHeaderWriter(PImageHeader);
 
 	return 0;
 }
--- README.md
+++ README.md
@@ -7,10 +7,13 @@ PE_dosyalarının yapısını anlamak ve içerisindeki bilgileri dışarı çık
 
 muraterdem.org 
 
-https://www.muraterdem.org/pe1/
-https://www.muraterdem.org/pe2/
+https://www.muraterdem.org/pe1/ 
+https://www.muraterdem.org/pe2/  
+https://www.muraterdem.org/p3/
 
 sibervatan.org 
 
-https://www.sibervatan.org/makale/pe-dosyalari-0x01/43
+https://www.sibervatan.org/makale/pe-dosyalari-0x01/43  
 https://www.sibervatan.org/makale/pe-dosyalari-0x02/44
+https://www.sibervatan.org/makale/pe-dosyalari-0x03/46 
+
